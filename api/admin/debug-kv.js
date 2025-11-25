fb9064d00a689705a1c71b3fec8556037b7b2360
bsrathore2527-ctrl <bsrathore2527@gmail.com>
2025-11-25 19:14:11 +0530

    Delete api/admin/debug-kv.js

--- api/admin/debug-kv.js
+++
@@ -1,9 +0,0 @@
-import { kv } from "../_lib/kv.js";
-
-export default async function handler(req, res) {
-  const raw = await kv.get("guardian:sell_orders");
-  return res.status(200).json({
-    raw,
-    typeof_raw: typeof raw
-  });
-}
