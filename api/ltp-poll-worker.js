78c253db37a6a42359f0863f90ff2e6c107338be
bsrathore2527-ctrl <bsrathore2527@gmail.com>
2025-12-02 21:31:53 +0530

    Update ltp-poll-worker.js

--- api/ltp-poll-worker.js
+++ api/ltp-poll-worker.js
@@ -3,7 +3,7 @@ import { kv } from "./_lib/kv.js";
 import { instance } from "./_lib/kite.js";
 
 export const config = {
-  runtime: "nodejs20",
+  runtime: "nodejs",
 };
 
 async function loadTradebookSymbols() {
