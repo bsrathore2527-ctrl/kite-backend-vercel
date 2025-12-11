78ac3d48836afa2f2e2fef33b207212aa413f975
bsrathore2527-ctrl <bsrathore2527@gmail.com>
2025-12-11 19:47:00 +0530

    Update kill.js

--- api/kill.js
+++ api/kill.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
   }
 
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type","x-admin-key");
 
   if (req.method === "OPTIONS") return res.status(200).end();
   if (req.method !== "POST")
