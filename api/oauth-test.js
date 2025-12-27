dc588b14a181255194980bca016ea539a787aa9b
bsrathore2527-ctrl <bsrathore2527@gmail.com>
2025-12-27 09:38:20 +0530

    Delete api/oauth-test.js

--- api/oauth-test.js
+++
@@ -1,9 +0,0 @@
-// api/oauth-test.js
-export default function handler(req, res) {
-  res.redirect(
-    "https://kite.zerodha.com/connect/login" +
-    "?v=3" +
-    `&api_key=${process.env.KITE_API_KEY}` +
-    "&state=HARD_PROOF"
-  );
-}
